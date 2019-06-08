78d02efca3ef78d9a73b99c6237a7d3780ca2492
raghuhoskote <raghuhoskote@gmail.com>
2019-06-08 12:04:30 +0530

    Update MyUserDetailsService.java

--- src/main/java/com/expense/service/impl/MyUserDetailsService.java
+++ src/main/java/com/expense/service/impl/MyUserDetailsService.java
@@ -14,7 +14,7 @@ import com.expense.entity.repository.UserRepository;
  */
 
 @Service
-public class MyUserDetailsService implements UserDetailsService {
+public class MyUserDetailsService  {
 
 	@Autowired
 	private UserRepository userRepository;
