2eb6c6f5f2ca84b288729fa36da4e6593c7a4694
chetands <51371219+chetands@users.noreply.github.com>
2019-06-08 11:51:45 +0530

    Update ExpenseServiceImpl.java

--- src/main/java/com/expense/service/impl/ExpenseServiceImpl.java
+++ src/main/java/com/expense/service/impl/ExpenseServiceImpl.java
@@ -29,11 +29,11 @@ public class ExpenseServiceImpl implements ExpenseService {
 	public Expense saveExpense(Expense expense) {
 		
 		//Uncomment below 2 lines to make test fail (ExpenseServiceTest.saveExpenseTest())
-		expense.setAmount(2000.00);
-		return expense;
+		//expense.setAmount(2000.00);
+		//return expense;
 		
 		//Comment below line to make test fail
-		//return expenseRepository.save(expense);
+		return expenseRepository.save(expense);
 	}
 
 	public List<Expense> getExpense() {
