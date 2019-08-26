481d468bab4ac3ec1b47c1112e78696987dbf297
umangsaltuniv <umang@saltuniv.com>
2019-08-26 19:36:36 +0530

    codecommit1.0

--- src/main/java/com/expense/service/impl/UserServiceImpl.java
+++ src/main/java/com/expense/service/impl/UserServiceImpl.java
@@ -66,12 +66,12 @@ public class UserServiceImpl implements UserService {
 	public User saveUser(User user) {
 		
 		// Buggy Code(UserServiceTest.saveUserTest())
-			user.setUsername("Uth");
-			return user;
+			//user.setUsername("Uth");
+			//return user;
 
 		// Correct Code
-			//user.setPassword(passwordEncoder.encode(user.getPassword()));
-			//return userRepository.save(user);
+			user.setPassword(passwordEncoder.encode(user.getPassword()));
+			return userRepository.save(user);
 
 	}
 
